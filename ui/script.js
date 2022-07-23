4b23bcec8e20bd0b1e66503769f24cc9e8585ba7
yilmazoncum <oncumkorkmaz@gmail.com>
2022-07-23 10:45:59 +0300

    add ui and whiteboard

---
+++ ui/script.js
@@ -0,0 +1,22 @@
+var canvas = document.getElementById('my-canvas');
+
+canvas.width = 0.98 * window.innerWidth;
+canvas.height = 0.9 * window.innerHeight;
+
+let ctx = canvas.getContext('2d');
+let x,y;
+let lineActive = false;
+
+canvas.addEventListener('click',(e) => {
+    lineActive = !lineActive
+    ctx.moveTo(x, y)
+})
+
+window.onmousemove = (e)=>{
+    x = e.clientX;
+    y = e.clientY;
+    if(lineActive){
+        ctx.lineTo(x,y)
+        ctx.stroke();
+    }
+}
