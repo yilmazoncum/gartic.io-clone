33842b64bd63495e69b5990a4a150f6131c96e1e
yilmazoncum <oncumkorkmaz@gmail.com>
2022-07-25 15:36:30 +0300

    merge solved

--- server/server.js
+++ server/server.js
@@ -1,39 +1,20 @@
-<<<<<<< HEAD
 const express = require('express')
 const app = express();
-=======
-const app = require('express')();
->>>>>>> 626d6bd1fa88cb1796a988a548ece9c7b6be95ae
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
     cors: {
         origin: '*',
     }
 });
-<<<<<<< HEAD
 app.use('/',express.static('ui'))
-=======
->>>>>>> 626d6bd1fa88cb1796a988a548ece9c7b6be95ae
 
 var connections = []
 
 io.on('connect', (socket) => {
-<<<<<<< HEAD
     
     connections.push(socket)
     console.log(`${socket.id} has connected`);
 
-=======
-
-    connections.push(socket)
-    console.log(socket);
-    console.log(`${socket.id} has connected`);
-
-   /*  socket.on('canvas-data', (data) => {
-        io.broadcast.emit('canvas-data', data);
-    }) */
-
->>>>>>> 626d6bd1fa88cb1796a988a548ece9c7b6be95ae
     socket.on('disconnect', () => {
         connections = connections.filter((cn) => cn.id !== socket.id);
         console.log(`${socket.id} is disconnected`);
