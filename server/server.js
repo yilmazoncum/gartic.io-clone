800de7fef35c5de3e654bbe2342372c2c80c08cd
yilmazoncum <oncumkorkmaz@gmail.com>
2022-10-22 15:42:49 +0300

    add room logic

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const io = require('socket.io')(http, {
         origin: '*',
     }
 });
+
 app.get('/', (req, res) => {
     res.sendFile(process.cwd()+ '/ui/login.html');
   });
@@ -19,7 +20,7 @@ var clientIdArr = [];
 var connectionsLimit = 100; //default limit
 
 io.on('connect', (socket) => {
-
+    
     if (io.engine.clientsCount > connectionsLimit) {
         message = 'Game is already started'
         io.emit('error',message)
@@ -31,15 +32,33 @@ io.on('connect', (socket) => {
     connections.push(socket)
     
     socket.on('add username', (username) => {
+        if (io.engine.clientsCount == 1 ) {
+            //ilk bağlanan kişi leader room'a
+            socket.join("leader")
+            io.emit('leaderChange')
+        }
+        else {
+            //diğerleri non-leader room'a
+            socket.join("non-leader")
+        }
+
         socket.username = username;
         console.log(`${socket.username} has connected`);
         clientIdArr.push({"id":socket.id,"username":socket.username})
         io.emit('update-client-count',clientIdArr);
     });
 
-    socket.on('disconnect', () => {
+    socket.on('disconnect', async () => {
         connections = connections.filter((cn) => cn.id !== socket.id);
         clientIdArr = clientIdArr.filter((cn) => cn.id !== socket.id);
+
+        //leader room'un client sayısı 
+        const sockets = await io.in("leader").fetchSockets();
+        if(sockets.length == 0){
+            newLeaderId = clientIdArr[0].id
+            io.to(newLeaderId).emit("leaderChange")
+        }
+
         console.log(`${socket.username} is disconnected`);
         io.emit('update-client-count', clientIdArr);
     })
@@ -57,6 +76,19 @@ io.on('connect', (socket) => {
         connectionsLimit = io.engine.clientsCount
     })
 
+    socket.on('leaderChange', async () =>{
+        socket.leave("non-leader")
+        socket.join("leader")
+    })
 })
+
+const GetSocketsInfo = async () => {
+    // Util/Debug Func
+    const sockets = await io.fetchSockets();
+    for (const socket of sockets) {
+        console.log(socket.id);
+        console.log(socket.rooms);
+      }
+}
 
 http.listen(5000, () => console.log('listening on port 5000'));
